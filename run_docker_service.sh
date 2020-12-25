4ac26bf3c21f30787f0acdc05346ccaac8e67b01
Shon Verch <nerftactic09@gmail.com>
2020-12-25 07:02:07 +0100

    run_docker_service.sh includes database URL

--- run_docker_service.sh
+++ run_docker_service.sh
@@ -1,2 +1,5 @@
 #/bin/bash
-docker run --env-file="./.env" -p 0.0.0.0:6969:6969 -d alfonstore
+docker run --env-file="./.env" -p 0.0.0.0:6969:6969 \
+	# TODO: Fix this! Hard coding db url probably not a good idea.
+	--env DATABASE_URL="postgres://alfonstore:saleor@136.243.107.6:5432/saleor"
+	-d alfonstore
